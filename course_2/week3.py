76ebdd23f7ae6b5b0c51164269ce71e87c608d80
Kaniska Mohanty <mohantyk@users.noreply.github.com>
2020-03-29 10:17:10 -0700

    Search for dna kmers encoding a particular peptide

--- course_2/week3.py
+++ course_2/week3.py
@@ -1,3 +1,8 @@
+import sys
+sys.path.append('..')
+
+from course_1.week2 import reverse_complement
+
 def get_rna_table():
     rna_amino_file = 'RNA_codon_table_1.txt'
     codon_to_amino = {}
@@ -24,3 +29,19 @@ def rna_to_protein(rna):
             peptide.append(amino_acid)
     protein = ''.join(peptide)
     return protein
+
+def dna_to_rna(dna):
+    return dna.replace('T','U')
+
+def peptide_to_dna(dna, peptide):
+    k = 3*len(peptide)
+    n = len(dna)
+    candidates = []
+    for i in range(n-k+1):
+        kmer = dna[i:i+k]
+        reverse_kmer = reverse_complement(kmer)
+        if ((rna_to_protein(dna_to_rna(kmer)) == peptide) or
+            (rna_to_protein(dna_to_rna(reverse_kmer)) == peptide)):
+            candidates.append(kmer)
+    return candidates
+
