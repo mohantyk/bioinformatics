7f43c1176632a4c4be9e6b52406134d6bab46d8f
Kaniska Mohanty <mohantyk@users.noreply.github.com>
2020-03-29 22:55:11 -0700

    Add function to trim leaderboard

--- course_2/week4.py
+++ course_2/week4.py
@@ -1,5 +1,7 @@
 from collections import Counter
 
+from week3 import spectrum
+
 def spectrum_score(peptide_spectrum, ref_spectrum):
     p_count = Counter(peptide_spectrum)
     r_count = Counter(ref_spectrum)
@@ -7,3 +9,12 @@ def spectrum_score(peptide_spectrum, ref_spectrum):
     for mass in p_count:
         score += min(p_count[mass], r_count[mass])
     return score
+
+def trim(peptides, ref_spectrum, N):
+    scores = Counter()
+    for peptide in peptides:
+        l_spectrum = spectrum(peptide, False)
+        score = spectrum_score(l_spectrum, ref_spectrum)
+        scores[peptide] = score
+    trimmed = [x[0] for x in scores.most_common(N)] # Does not account for ties
+    return trimmed
