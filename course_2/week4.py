9ab8d9bee1b40308c962af956c744390f9add599
Kaniska Mohanty <mohantyk@users.noreply.github.com>
2020-03-30 15:13:22 -0700

    Add leaderboard based cyclopeptide sequencing

--- course_2/week4.py
+++ course_2/week4.py
@@ -1,6 +1,12 @@
 from collections import Counter
 
-from week3 import spectrum
+import logging
+logging.basicConfig()
+week4_logger = logging.getLogger('week4')
+week4_logger.setLevel(logging.ERROR)
+
+from week3 import spectrum, consistent
+from week3 import MASS_2_AMINO
 
 def spectrum_score(peptide_spectrum, ref_spectrum):
     p_count = Counter(peptide_spectrum)
@@ -18,3 +24,30 @@ def trim(peptides, ref_spectrum, N):
         scores[peptide] = score
     trimmed = [x[0] for x in scores.most_common(N)] # Does not account for ties
     return trimmed
+
+
+def leaderboard_cyclopeptide_sequencing(mass_spectrum, N):
+    leaderboard = set([''])
+    leader_peptide = ''
+    parent_mass = mass_spectrum[-1]
+    while leaderboard:
+        leaderboard = set(candidate+aa for candidate in leaderboard
+                                        for aa in MASS_2_AMINO.values())
+        leader_spectrum = spectrum(leader_peptide, cyclic=True)
+        candidates = leaderboard.copy()
+        for candidate in candidates:
+            c_spectrum = spectrum(candidate, cyclic=True)
+            l_spectrum = spectrum(candidate, cyclic=False)
+            peptide_mass = l_spectrum[-1]
+            if peptide_mass == parent_mass:
+                candidate_score = spectrum_score(c_spectrum, mass_spectrum)
+                leader_score = spectrum_score(leader_spectrum, mass_spectrum)
+                week4_logger.debug(f'{candidate} : {candidate_score} <> {leader_peptide} : {leader_score}')
+                if candidate_score > leader_score:
+                    leader_peptide = candidate
+                    leader_spectrum = spectrum(leader_peptide, cyclic=True)
+            elif peptide_mass > parent_mass:
+                leaderboard.remove(candidate)
+        leaderboard = trim(leaderboard, mass_spectrum, N)
+
+    return leader_peptide
