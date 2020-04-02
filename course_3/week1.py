81246e4549ac5933b830d170b7b35bd8413b7ed8
Kaniska Mohanty <mohantyk@users.noreply.github.com>
2020-04-02 14:21:21 -0700

    Allow for diagonal paths in manhattan

--- course_3/week1.py
+++ course_3/week1.py
@@ -17,9 +17,10 @@ def dp_change(money, coins):
     return results[money]
 
 
-def manhattan_tourist(n, m, down_matrix, right_matrix):
+def manhattan_tourist(n, m, down_matrix, right_matrix, diag_matrix=None):
     assert right_matrix.shape == (n+1, m)
     assert down_matrix.shape == (n, m+1)
+    assert diag_matrix is None or diag_matrix.shape == (n, m)
 
     longest_path = np.zeros((n+1, m+1), dtype=int)
     for j in range(1,m+1):
@@ -28,7 +29,13 @@ def manhattan_tourist(n, m, down_matrix, right_matrix):
         longest_path[i, 0] = longest_path[i-1, 0] + down_matrix[i-1, 0]
     for i in range(1, n+1):
         for j in range(1, m+1):
-            longest_path[i, j] = max(longest_path[i, j-1] + right_matrix[i, j-1],
-                                 longest_path[i-1, j] + down_matrix[i-1, j])
+            from_left = longest_path[i, j-1] + right_matrix[i, j-1]
+            from_top = longest_path[i-1, j] + down_matrix[i-1, j]
+            if diag_matrix is not None:
+                from_corner = longest_path[i-1, j-1] + diag_matrix[i-1, j-1]
+            else:
+                from_corner = 0
+
+            longest_path[i, j] = max(from_left, from_top, from_corner)
 
     return longest_path[n, m]
