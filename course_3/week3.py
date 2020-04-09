7360fde809136de5d4380f83f1fbef02fd44a49d
Kaniska Mohanty <mohantyk@users.noreply.github.com>
2020-04-09 13:57:07 -0700

    First draft of affine backtracking

--- course_3/week3.py
+++ course_3/week3.py
@@ -8,16 +8,72 @@ def align_with_affine_gap_penalty(v, w, σ, ε, score_table=BLOSUM62):
     middle = np.zeros((n+1, m+1), dtype=int)
     upper = np.zeros((n+1, m+1), dtype=int)
 
+    backtrack_l = np.empty((n+1, m+1), dtype=str)
+    backtrack_m = np.empty((n+1, m+1), dtype=str)
+    backtrack_u = np.empty((n+1, m+1), dtype=str)
+
     for i in range(n+1):
         for j in range(m+1):
             if i>0:
                 lower[i, j] = max(lower[i-1, j]-ε, middle[i-1, j]-σ)
+                if lower[i, j] == lower[i-1, j]-ε:
+                    backtrack_l[i, j] = 'N' # North
+                else:
+                    backtrack_l[i, j] = 'M' # Middle
             if j>0:
                 upper[i, j] = max(upper[i, j-1]-ε, middle[i, j-1]-σ)
+                if upper[i, j] == upper[i, j-1]-ε:
+                    backtrack_u[i, j] = 'W'
+                else:
+                    backtrack_u[i, j] = 'M'
             if i>0 and j>0:
                 middle[i, j] = max(lower[i,j], upper[i,j],
                                     middle[i-1,j-1] + score_table.loc[v[i-1], w[j-1]])
+                if middle[i, j] == lower[i, j]:
+                    backtrack_m[i, j] = 'L'
+                elif middle[i, j] == lower[i, j]:
+                    backtrack_m[i, j] = 'U'
+                else:
+                    backtrack_m[i, j] = 'NW'
+    final_score = middle[n, m]
+    curr = backtrack_m
+    i, j = n, m
+    rev_w = []
+    rev_v = []
+    while (i,j) != (0, 0):
+        direction = curr[i, j]
+        if direction == 'N':
+            rev_v.append(v[i-1])
+            rev_w.append('-')
+            i -= 1
+        elif direction == 'W':
+            rev_v.append('-')
+            rev_w.append(w[j-1])
+            j -= 1
+        elif direction == 'NW':
+            rev_v.append(v[i-1])
+            rev_w.append(w[j-1])
+            i -= 1
+            j -= 1
+        elif direction == 'U':
+            curr = backtrack_u
+        elif direction == 'L':
+            curr = backtrack_l
+        elif direction == 'M': # Switch from lower/upper to middle grid
+            if curr is backtrack_l:
+                rev_v.append(v[i-1])
+                rev_w.append('-')
+                i -= 1
+            elif curr is backtrack_u:
+                rev_v.append('-')
+                rev_w.append(w[j-1])
+                j -= 1
+            curr = backtrack_m
+        else:
+            raise ValueError('Unexpected if branch')
 
-    return middle[n, m]
+    align_v = ''.join(rev_v[::-1])
+    align_w = ''.join(rev_w[::-1])
+    return final_score, align_v, align_w
 
 
