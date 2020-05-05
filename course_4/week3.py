a66b92876f13d51934f8d067789d29bf757f7057
Kaniska Mohanty <mohantyk@users.noreply.github.com>
2020-05-05 13:35:53 -0700

    First draft of small parsimony score

--- course_4/week3.py
+++ course_4/week3.py
@@ -1,3 +1,6 @@
+from math import inf
+
+alphabet = ['A', 'C', 'G', 'T']
 class Node:
     def __init__(self, val, left=None, right=None):
         self.val = val
@@ -6,5 +9,22 @@ class Node:
 
     def is_leaf(self):
         return (not self.left) and (not self.right)
+
+
+
+def small_parsimony_score(node, idx):
+    score = {letter:inf for letter in alphabet}
+    if node.is_leaf():
+        ch = node.val[idx]
+        score[ch] = 0
+        return score
+
+    lscore = small_parsimony_score(node.left, idx)
+    rscore = small_parsimony_score(node.right, idx)
+    for k in alphabet:
+        score[k] = min(lscore[i] + k!=i for i in lscore) + min(rscore[j] + k!=j for j in lscore)
+    return score
+
+
 
 
