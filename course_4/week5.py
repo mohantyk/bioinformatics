a50b11765ea2cdf6fd4eae48e679e46db2d2e6f5
Kaniska Mohanty <mohantyk@users.noreply.github.com>
2020-05-12 19:10:03 -0700

    Remove extra node in path

--- course_4/week5.py
+++ course_4/week5.py
@@ -6,6 +6,11 @@ import sys
 sys.path.append('..')
 from course_2.week3 import AMINO_MASS, MASS_2_AMINO
 
+import logging
+logging.basicConfig()
+logger = logging.getLogger('week5')
+logger.setLevel(logging.ERROR)
+
 def peptide_identification(spectral_vec, proteome, amino_mass=AMINO_MASS):
     graph = DirectedGraph()
     graph.add_node(0)
@@ -15,6 +20,7 @@ def peptide_identification(spectral_vec, proteome, amino_mass=AMINO_MASS):
         graph.add_node(i)
         graph.add_weight(i, s_i)
     m = i
+    logger.debug(f'm = {m}')
 
     best_score = -inf
     best_path = []
@@ -23,23 +29,30 @@ def peptide_identification(spectral_vec, proteome, amino_mass=AMINO_MASS):
         score = 0
         idx = start_idx
         path = []
+        nodes = [] # Debug
 
+        logger.debug(f'Starting from {start_idx}')
         while True:
+            nodes.append(curr_node) # Debug
+
             amino = proteome[idx]
             path.append(amino)
             score += graph.weights[curr_node]
             mass = amino_mass[amino]
 
             if curr_node == m:
+                mypath = ''.join(path)
+                logger.debug(f'{start_idx}, {score}, {mypath}')
                 if score > best_score:
                     best_score = score
-                    best_path = path[:]
+                    best_path = path[:-1] # One extra amino acid is added while processign last node
                     break
 
             curr_node = curr_node + mass
             idx += 1
             if curr_node > m or idx >= len(proteome):
                 break
+        logger.debug(f'{nodes}\n')
 
     return ''.join(best_path)
 
