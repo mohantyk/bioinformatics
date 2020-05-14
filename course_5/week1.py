8c137a1fbb233a428176d2819eb06a1cc39b030d
Kaniska Mohanty <mohantyk@users.noreply.github.com>
2020-05-14 10:42:22 -0700

    First draft of lloyd

--- course_5/week1.py
+++ course_5/week1.py
@@ -1,10 +1,20 @@
 from math import sqrt, inf
 import random
 
+
 def euclidean_dist(p1, p2):
     assert len(p1) == len(p2)
     dist = sqrt(sum((x1-x2)**2 for x1, x2 in zip(p1, p2)))
     return dist
+
+def center_of_gravity(points):
+    n = len(points)
+    m = len(points[0])
+    center = []
+    for idx in range(m):
+        center.append(sum(pt[idx] for pt in points)/n)
+    return tuple(center)
+
 
 def farthest_first_traversal(k, m, points):
     '''
@@ -35,4 +45,19 @@ def squared_error_distortion(k, m, centers, points):
 
 
 def lloyd(k, m, points):
-    pass
+    centers = set(points[:k])
+
+    while True:
+        clusters = {center: set() for center in centers}
+        for pt in points:
+            chosen = min(centers, key = lambda center: euclidean_dist(center, pt))
+            clusters[chosen].add(pt)
+        new_centers = {center_of_gravity(list(points)) for points in clusters.values()}
+        if new_centers == centers:
+            break
+        else:
+            centers = new_centers
+
+    return list(centers)
+
+
