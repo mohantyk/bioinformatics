3baf9ee52dd97fa411f4f2b1ffe24569ad01a0ab
Kaniska Mohanty <mohantyk@users.noreply.github.com>
2020-05-14 09:11:58 -0700

    Implement farthest first traversal

--- course_5/test_course5.py
+++ course_5/test_course5.py
@@ -4,7 +4,7 @@ class TestWeek1:
     def test_farthest_first_traversal(self):
         k = 3
         m = 2
-        points = {(0, 0), (5, 5), (0, 5), (1, 1), (2, 2), (3, 3), (1, 2)}
+        points = [(0, 0), (5, 5), (0, 5), (1, 1), (2, 2), (3, 3), (1, 2)]
 
         expected_centers  = {(0,0), (5,5), (0,5)}
         assert farthest_first_traversal(k, m, points) == expected_centers
--- course_5/week1.py
+++ course_5/week1.py
@@ -1,2 +1,25 @@
+from math import sqrt, inf
+import random
+
+def euclidean_dist(p1, p2):
+    assert len(p1) == len(p2)
+    dist = sqrt(sum((x1-x2)**2 for x1, x2 in zip(p1, p2)))
+    return dist
+
 def farthest_first_traversal(k, m, points):
-    pass
+    '''
+    Returns centers of k clusters
+    '''
+    assert len(points[0]) == m
+    centers = {points[0]}
+    while len(centers) < k:
+        farthest_dist = 0
+        for pt in points:
+            if pt in centers:
+                continue
+            dist = min(euclidean_dist(pt, center) for center in centers)
+            if dist > farthest_dist:
+                farthest_dist = dist
+                farthest_pt = pt
+        centers.add(farthest_pt)
+    return centers
