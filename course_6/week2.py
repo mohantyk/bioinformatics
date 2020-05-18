82ffae6c27e9ac2c4949e3e7fd350594acfd0d5f
Kaniska Mohanty <mohantyk@users.noreply.github.com>
2020-05-18 11:44:19 -0700

    Implement inverse of bwt

--- course_6/week2.py
+++ course_6/week2.py
@@ -10,4 +10,27 @@ def burrows_wheeler_transform(text):
     return bwt
 
 def invert_bwt(bwt):
-    pass
+    last_col = bwt
+    first_col = ''.join(sorted(bwt))
+
+    first_row = '$'
+    last_ltr_count = 0
+    for row_idx in range(1, len(bwt)-1):
+        last_ltr = first_row[-1]
+
+        # Find col idx of #last_ltr_count occurence of last_ltr in last_col
+        count = 0
+        for col_idx, ch in enumerate(last_col):
+            if ch == last_ltr:
+                if count == last_ltr_count:
+                    break
+                else:
+                    count += 1
+        nxt_ltr = first_col[col_idx]
+        first_row = first_row + nxt_ltr
+        #Find count of nxt_ltr
+        last_ltr_count = len([ch for ch in first_col[:col_idx] if ch==nxt_ltr])
+
+    first_row = first_row + last_col[0] # Add the last character of first row
+    text = first_row[1:] + first_row[0] # Move the $ to the end
+    return text
