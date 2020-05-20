e4c25f922c0510188c80b984254bc1c510452c66
Kaniska Mohanty <mohantyk@users.noreply.github.com>
2020-05-20 11:51:14 -0700

    Implement hmm path

--- course_6/week5.py
+++ course_6/week5.py
@@ -1,2 +1,37 @@
+from collections import defaultdict
+
 def create_profile_hmm(alignment, threshold, alphabet):
-    return None, None
+    star_alignment = set()
+    num_patterns = len(alignment)
+    for idx, _ in enumerate(alignment[0]):
+        deletions = sum(1 for pattern in alignment
+                            if pattern[idx] == '-')
+        if deletions <= threshold * num_patterns:
+            star_alignment.add(idx)
+
+    paths = []
+    for pattern in alignment:
+        path = ['S']
+        curr = 0
+        for idx, ltr in enumerate(pattern):
+            if idx in star_alignment:
+                curr += 1
+                if ltr != '-':
+                    path.append('M'+str(curr)) # Letter emitted
+                else:
+                    path.append('D'+str(curr)) # Deletion
+
+            else:
+                if ltr != '-':
+                    path.append('I'+str(curr)) # Insertion
+        path.append('E')
+        paths.append(path)
+
+
+
+
+if __name__ == '__main__':
+    threshold = 0.289
+    alphabet = 'ABCDE'
+    multiple_alignment = ['EBA', 'E-D', 'EB-', 'EED', 'EBD', 'EBE', 'E-D', 'E-D']
+    create_profile_hmm(multiple_alignment, threshold, multiple_alignment)
